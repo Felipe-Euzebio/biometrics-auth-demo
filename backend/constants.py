a8b1d076050ade317280864cccfa9effc8a250d4
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2025-06-14 13:32:20 -0300

    Add minimum and maximum image resolution constants for facial recognition

--- backend/constants.py
+++ backend/constants.py
@@ -6,5 +6,11 @@ ALLOWED_IMAGE_FORMATS = ["png", "jpeg"]
 # Maximum allowed image size in MB for user biometric profile
 MAX_IMAGE_SIZE_MB = 5
 
+# Minimum image resolution for facial recognition (width x height)
+MIN_IMAGE_RESOLUTION = (100, 100)
+
+# Maximum image resolution for facial recognition (width x height)
+MAX_IMAGE_RESOLUTION = (4096, 4096)
+
 # Default model name for facial embeddings
 DEFAULT_MODEL_NAME = "Facenet512"
