8b59b719c0e61b6ff130c4f412aa50df87080b3b
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2025-06-16 18:40:26 -0300

    Update __init__.py to include LoginResponseDto in schemas package exports

--- backend/schemas/__init__.py
+++ backend/schemas/__init__.py
@@ -2,5 +2,6 @@
 from .UserDto import UserDto
 from .RegisterDto import RegisterDto
 from .LoginDto import LoginDto
+from .LoginResponseDto import LoginResponseDto
 
-__all__ = ["UserDto", "RegisterDto", "LoginDto"]
+__all__ = ["UserDto", "RegisterDto", "LoginDto", "LoginResponseDto"]
