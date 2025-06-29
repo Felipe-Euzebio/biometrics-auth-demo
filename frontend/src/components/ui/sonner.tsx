71f04d9cec463eb8823c32b6560b259532895fcd
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2025-06-29 20:07:11 -0300

    Include export of custom toast functions in the Toaster component

--- frontend/src/components/ui/sonner.tsx
+++ frontend/src/components/ui/sonner.tsx
@@ -1,7 +1,8 @@
 "use client"
 
+import { ValidationError } from "@/types/api"
 import { useTheme } from "next-themes"
-import { Toaster as Sonner, ToasterProps } from "sonner"
+import { Toaster as Sonner, toast, ToasterProps } from "sonner"
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme()
@@ -22,4 +23,19 @@ const Toaster = ({ ...props }: ToasterProps) => {
   )
 }
 
-export { Toaster }
+const customToasts = {
+  validationError: (message: string, errors: ValidationError[]) => 
+    toast.error(message, {
+      description: (
+        <ul className="list-disc list-inside space-y-1">
+          {errors.map(({ loc, msg }, index) => (
+            <li key={index}>
+              <span className="font-medium">{loc.at(-1) || 'Field'}:</span> {msg}
+            </li>
+          ))}
+        </ul>
+      )
+    })
+}
+
+export { Toaster, customToasts }
