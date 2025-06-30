3c2d9352dde06fd3ff7682bab010bb728c8837a8
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2025-06-30 20:31:19 -0300

    Refactor and introduce new types in types/api.ts

--- frontend/src/types/api.ts
+++ frontend/src/types/api.ts
@@ -1,9 +1,20 @@
+import { SWRConfiguration } from "swr";
+
 export type ValidationError = {
   loc: string[];
   msg: string;
   type: string;
 }
 
-export type ErrorResponse = {
-  detail: string | ValidationError[];
+export type ApiError = {
+  detail: string | ValidationError[]
 }
+
+export type QueryParams = Record<string, any>;
+
+export type FetchHookOptions = {
+  query?: QueryParams;
+  config?: SWRConfiguration;
+};
+
+export type HttpMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE"
