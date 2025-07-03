8ca2bd162e3e833cb3e9a5f705f0f38b35bfee9c
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2025-07-03 14:37:24 -0300

    Add user store using Zustand for state management
    This commit introduces a new user store implemented with Zustand, allowing for user state management including setting the user and handling logout functionality. The store is also configured to persist user data in local storage.

---
+++ frontend/src/store/user-store.ts
@@ -0,0 +1,24 @@
+import { User } from "@/types/user";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface UserState {
+  user: User | null;
+  setUser: (user: User | null) => void;
+  logout: () => void;
+}
+
+const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+      logout: () => set({ user: null }),
+    }),
+    {
+      name: "user-storage",
+    }
+  )
+);
+
+export default useUserStore;
